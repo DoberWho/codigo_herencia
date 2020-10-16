f80b17fd8f015cdc55f57a800c51c9eb91740414
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-10-16 12:41:47 +0200

    Ejemplo de Testing

--- maven.tienda/src/test/java/com/castelaofpe/maven/tienda/AppTest.java
+++ maven.tienda/src/test/java/com/castelaofpe/maven/tienda/AppTest.java
@@ -1,5 +1,8 @@
 package com.castelaofpe.maven.tienda;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +16,10 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
-    }
+        assertFalse(true);
+        assertNotEquals(null, null); 
+    } 
+    
 }
